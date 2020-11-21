a5cd15c1d186248ab5b08b4fa2abe214a4d207b1
BugenZhao <i@bugenzhao.com>
2020-11-21 16:34:23 +0800

    add sample dns lookup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 #[cfg_attr(test, macro_use)]
 extern crate lazy_static;
-#[macro_use]
+#[cfg_attr(dns_packet, macro_use)]
 extern crate num_derive;
 
 mod dns_packet;
@@ -10,6 +10,23 @@ mod dns_packet_buf;
 mod error;
 mod utils;
 
+use dns_packet::DnsPacket;
+use dns_packet_buf::DnsPacketBuf;
+use std::net::UdpSocket;
+
 fn main() {
-    println!("Hello, world!");
+    let ns_server = ("223.5.5.5", 53);
+    let socket = UdpSocket::bind(("0.0.0.0", 0)).unwrap();
+    socket.connect(ns_server).unwrap();
+
+    let packet = DnsPacket::example("bugen.dev");
+    let mut send_buf = DnsPacketBuf::new();
+    packet.write(&mut send_buf).unwrap();
+    socket.send(&send_buf.buf[0..send_buf.pos]).unwrap();
+
+    let mut recv_buf = DnsPacketBuf::new();
+    let (_, _response_server) = socket.recv_from(&mut recv_buf.buf).unwrap();
+    let response_packet = DnsPacket::read_from(&mut recv_buf).unwrap();
+
+    println!("Received from {} => {:#?}", _response_server, response_packet);
 }
